652d1154f00b158f5cec2f790b4b43d8ab6a2112
Davidecpp <cppdvd03d13m208q@studenti.unical.it>
2025-05-29 16:50:49 +0200

    fixes

--- Assets/Scripts/MovingObstacle.cs
+++ Assets/Scripts/MovingObstacle.cs
@@ -1,51 +1,74 @@
 using UnityEngine;
 using UnityEngine.AI;
-using Pathfinding;
 using Unity.AI.Navigation;
+using Pathfinding;
 
+[RequireComponent(typeof(Collider))]
 public class MovingObstacle : MonoBehaviour
 {
     public Vector3 startPosition;
     public Vector3 endPosition;
     public float speed = 2f;
+
     public NavMeshSurface navMeshSurface;
 
     public delegate void OnNavMeshUpdated();
     public static event OnNavMeshUpdated NavMeshUpdated;
 
-    public float updateInterval = 0.5f;
+    public float updateInterval = 2f; // Intervallo di aggiornamento consigliato
     private float timer = 0f;
     private Vector3 lastPosition;
+
+    private Collider obstacleCollider;
 
     void Start()
     {
         startPosition = transform.position;
-        endPosition = startPosition + new Vector3(5, 0, 0);
+        endPosition = startPosition + new Vector3(5f, 0f, 0f);
         lastPosition = transform.position;
+
+        obstacleCollider = GetComponent<Collider>();
+        if (obstacleCollider == null)
+        {
+            Debug.LogError("MovingObstacle richiede un Collider!");
+        }
     }
 
     void Update()
     {
-        // Muovi l'ostacolo tra startPosition e endPosition
+        // Movimento oscillante tra startPosition ed endPosition
         transform.position = Vector3.Lerp(startPosition, endPosition, Mathf.PingPong(Time.time * speed, 1));
 
         timer += Time.deltaTime;
+
         if (timer >= updateInterval && Vector3.Distance(transform.position, lastPosition) > 0.1f)
         {
             timer = 0f;
             lastPosition = transform.position;
 
-            // Aggiorna A*
-            if (TryGetComponent(out Collider col))
-                AstarPath.active.UpdateGraphs(col.bounds);
-
-            // Aggiorna NavMesh
-            if (navMeshSurface != null)
-            {
-                navMeshSurface.UpdateNavMesh(navMeshSurface.navMeshData);
-                NavMeshUpdated?.Invoke(); // Notifica gli NPC
-            }
+            UpdateAStarGraph();
+            UpdateNavMesh();
         }
     }
 
+    void UpdateAStarGraph()
+    {
+        if (AstarPath.active != null && obstacleCollider != null)
+        {
+            Bounds bounds = obstacleCollider.bounds;
+            GraphUpdateObject guo = new GraphUpdateObject(bounds);
+            AstarPath.active.UpdateGraphs(guo);
+            Debug.Log("A* Graph aggiornato");
+        }
+    }
+
+    void UpdateNavMesh()
+    {
+        if (navMeshSurface != null)
+        {
+            navMeshSurface.UpdateNavMesh(navMeshSurface.navMeshData);
+            Debug.Log("NavMesh aggiornato");
+            NavMeshUpdated?.Invoke();
+        }
+    }
 }
